9b876c504ef762d23867fc20833f35e5796654c1
Alexandre <alexandre.yukon@gmail.com>
2021-07-24 19:40:36 -0300

    Finalizando projeto com pair programming Ingrid e Alexandre

--- whats-lab/src/components/MessagesList.js
+++ whats-lab/src/components/MessagesList.js
@@ -2,32 +2,52 @@ import React from "react";
 import styled from "styled-components";
 
 const MessagesContainer = styled.div`
-  background-color: lavender;
+  background-color: white;
   width: 45vw;
   height: 90vh;
   margin-top: 10px;
+  margin-bottom: 10px;
   display: flex;
   flex-direction: column-reverse;
-  border-radius: 40px 0;
+  border-radius: 5px;
   border: 1px solid lightgray;
+
+  @media screen and (min-device-width : 320px) and (max-device-width : 480px) {
+/* celulares */
+  display: flex;
+}
+
 `;
+
 const Message = styled.p`
   background-color: #075E54;
   margin: 3px;
   width: 50%;
   padding: 12px 15px;
-  border-radius: 0 30px;
+  border-radius: 6px;
   border: 1px solid lightgray;
+
+  @media screen and (min-device-width : 320px) and (max-device-width : 480px) {
+/* celulares */
+  display: flex;
+}
+
 `;
 const MyMessage = styled.p`
   align-self: flex-end;
-  background-color: white;
+  background-color: red;
   margin: 3px;
   padding: 12px 15px;
   text-align: right;
   width: 50%;
-  border-radius: 30px 0;
+  border-radius: 5px;
   border: 1px solid lightgray;
+
+  @media screen and (min-device-width : 320px) and (max-device-width : 480px) {
+/* celulares */
+  display: flex;
+}
+
 `;
 
 class MessagesList extends React.Component {
