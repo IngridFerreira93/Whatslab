f753e9dc2ff7d59f16200e7d3131399a7896217f
Alexandre <alexandre.yukon@gmail.com>
2021-07-24 18:04:26 -0300

    Estilizando Whats-lab

--- whats-lab/src/App.js
+++ whats-lab/src/App.js
@@ -1,22 +1,21 @@
-import './App.css';
+import React from "react";
 import styled from "styled-components";
 import CreateMessage from "./components/CreateMessage";
 
-
-const Container = styled.div`
-  background-color: green;
+const MainContainer = styled.div`
+  background-color: black;
   height: 100vh;
   width: 100vw;
   display: flex;
   flex-direction: column;
   align-items: center;
-`
+`;
 
 function App() {
   return (
-    <Container>
+    <MainContainer>
       <CreateMessage />
-    </Container>
+    </MainContainer>
   );
 }
 
